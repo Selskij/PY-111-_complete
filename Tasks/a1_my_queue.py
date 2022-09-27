a2cb4778c73e0f39906e542cbcfbbef97474f717
Seslkij <selskij.v@edu.spbstu.ru>
2022-09-27 16:48:56 +0300

    a-section is done

--- Tasks/a1_my_queue.py
+++ Tasks/a1_my_queue.py
@@ -3,36 +3,60 @@ My little Queue
 """
 from typing import Any
 
+#append -> O(1)
+#pop(-1) -> O(1)
+#insert() -> O(N)
+#del() -> O(N)
 
-def enqueue(elem: Any) -> None:
+'''enqueue - O(N)
+    dequeue - O(1)'''
+
+queue = []
+#queue_priority = []
+
+def enqueue(elem: Any) -> None: # Как append а значит O(1)
     """
     Operation that add element to the end of the queue
 
     :param elem: element to be added
     :return: Nothing
     """
-    print(elem)
+
+
+    #reversed_index = -ind - 1
+    queue.insert(0, elem)
+    #queue.append(elem)
+    #reversed_queue = reversed(queue)
+    print(queue)
     return None
 
 
-def dequeue() -> Any:
+def dequeue() -> Any: # O(N) т.к извлекаем из конца и двигаем все элементы
     """
     Return element from the beginning of the queue. Should return None if no elements.
 
     :return: dequeued element
     """
-    return None
+    if not queue:
+        return None
+    return queue.pop()
+    #return None
 
 
-def peek(ind: int = 0) -> Any:
+def peek(ind: int = 0) -> Any: #O(1)
     """
     Allow you to see at the element in the queue without dequeuing it
 
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    print(ind)
-    return None
+    try:
+        queue[ind]
+    except IndexError:
+        return None
+    #print(ind)
+    reversed_index = -ind - 1
+    return queue[reversed_index]
 
 
 def clear() -> None:
@@ -41,4 +65,5 @@ def clear() -> None:
 
     :return: None
     """
+    queue.clear()
     return None

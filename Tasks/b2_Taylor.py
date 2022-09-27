002aac2c6858b8f7cb008be90a892a491e169326
Seslkij <selskij.v@edu.spbstu.ru>
2022-09-27 16:52:15 +0300

    b2-done

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,7 +2,24 @@
 Taylor series
 """
 from typing import Union
+import math
+from itertools import count
 
+EPSILON = 0.0001
+
+def get_item_n(x,n):
+    return x ** n / math.factorial(n)
+
+def get_si(x,n):
+    return ((-1)**n) * (x ** (2*n + 1)) / math.factorial((2*n)+1)
+
+# def get_sin_x(x):
+#     sum_ = 0
+#     for i in count(1,1):
+#         current_item = get_item_n(x,i)
+#         sum_+= current_item
+#         if abs(current_item) <= EPSILON:
+#             return sum_
 
 def ex(x: Union[int, float]) -> float:
     """
@@ -11,8 +28,13 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    sum_ = 1
+    for n in count(1, 1):
+        current_item = get_item_n(x,n)
+        sum_ += current_item
+        if current_item <= EPSILON:
+            break
+    return sum_
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +44,13 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    sum_ = 0
+    for n in count(0, 1):
+        current_item = get_si(x, n)
+        print(current_item)
+        sum_ += current_item
+
+        if abs(current_item) <= EPSILON:
+            break
+    return sum_
+

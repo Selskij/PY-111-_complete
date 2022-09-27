2e74cfaf3dc4d9f3773f2557e7d8704d168ba213
Seslkij <selskij.v@edu.spbstu.ru>
2022-09-27 16:54:36 +0300

    с1-done

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -15,5 +15,13 @@ def factorial_iterative(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    prev_ = n
+    for i in range(1,n):  # либо range (n-1)
+        current_item =prev_ * (n-i)
+        #a, b = b, b-1
+        prev_ = current_item
+        print(prev_)
+    #print(n)
+    return prev_

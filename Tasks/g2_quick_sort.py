51585830bc981cb2ac3c80b2d825d9e642bc0335
Seslkij <selskij.v@edu.spbstu.ru>
2022-09-29 16:44:50 +0300

    g1 with comments 2

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -13,7 +13,13 @@ from random import choice, randint
 #     if not container:
 #         return container
 #
-#     pivot = choice(container)
+#     pivot = choice(container) # Неким образом определяем опорный элемент
+#
+
+
+# Три слогаемых : Первое - рекурсия сортировки для элементов меньше начального,
+# второе - для элементов равных начальному, третье - рекурсия для элементов больше начального
+#
 #
 #     return sort([v for v in container if v < pivot]) \
 #            + [v for v in container if v == pivot] \
@@ -32,24 +38,30 @@ def sort(container: List[int]) -> List[int]:
             return
 
         random_index = randint(left_border, right_border)
-        pivot = container[random_index]
+        pivot = container[random_index] # Неким образом определяем опорный элемент
 
-        i, j = left_border, right_border
+        i, j = left_border, right_border # Идем по массиву двумя счетчиками – слева направо и справа налево
 
+
+# Всё что больше pivot после первого прохода займёт позиции справа, после чего сам pivot
+# перемещается направо и становится на позицию меньшего из новой последовательности
+# После этого операция повторяется заново
 
         while i <= j:
             while container[i] < pivot:
-                i += 1
+                i += 1 # Увеличиваем левый счетчик, пока он не встретит элемент больше или равный опорному
 
             while container[j] > pivot:
-                j -= 1
+                j -= 1 # Уменьшаем правый счетчик, пока он не встретит элемент меньше или равный опорному
 
             if i <= j: # если i и j пересеклись - производим обмен
-                container[i], container[j] = container[j], container[i]
+                container[i], container[j] = container[j], container[i] # Меняем элементы местами
                 i += 1
                 j -= 1
-        _sort(left_border, j)
-        _sort(i, right_border)
+# Продолжаем уменьшать счетчики и менять элементы аналогичным образом, пока счетчики не встретятся\
+# – получаем индекс элемента, левее которого все элементы меньше или равны опорному, а правее – больше опорного
+        _sort(left_border, j) # Запускаем рекурсивно алгоритм от части левее опорного элемента
+        _sort(i, right_border) # и правее опорного элемента
     _sort(0, len(container) - 1)
     return container
 
